d40696e3f1a8a77fc67839bed59b0c6c2480b7b1
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 02:22:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.nil?
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     puts "The line is currently: #{katz_deli}"
