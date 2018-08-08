a523e6d552d77e230d6f24865e4549d22c087226
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 02:43:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
 def now_serving(katz_deli)
