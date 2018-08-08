d853c6235575c0b0040ddde426f67f0ce4f0479d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 02:46:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    katz_deli.shift    
+  end
 end
